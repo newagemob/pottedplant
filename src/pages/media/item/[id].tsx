dc2143e94e2a97dce978634387b0d41f3b6b5de4
newagemob <isaiah.wright@tutanota.com>
2023-02-20 00:30:05 -0600

    styling [id] route

--- src/pages/media/item/[id].tsx
+++ src/pages/media/item/[id].tsx
@@ -1,40 +1,51 @@
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Loading } from '../../../components'
 import { trpc } from '../../../utils/trpc'
 
 const MediaItem: NextPage = () => {
+  const [isLoading, setIsLoading]: [boolean, React.Dispatch<React.SetStateAction<boolean>>] = useState(true)
+
   const router = useRouter()
   const { id } = router.query as { id: string }
 
   const { data: mediaData } = trpc.media.getMediaItem.useQuery({ id })
 
-  console.log(`👉 ${JSON.stringify(mediaData)}`)
+  useEffect(() => {
+    if (mediaData) {
+      setIsLoading(false)
+    }
+  }, [mediaData])
 
   return (
     <>
-      <div className='flex flex-col items-center justify-center w-full p-4 space-y-4 border border-gray-300 rounded-md'>
-        <div className='flex flex-row items-center justify-around w-full'>
-          <h1 className='text-4xl text-black'>
-            {mediaData?.name}
-          </h1>
+      {isLoading ? <Loading /> : (
+        <div className='flex flex-col items-center justify-center w-[100vw] h-[100vh] p-4 space-y-4'>
+          <div className='flex flex-row items-end justify-around md:justify-between justify-items-between w-full md:w-1/2 lg:w-1/3 mb-4'>
+            <h1 className='text-3xl text-slate-700 border-b-2 border-slate-700 font-medium text-left pb-1'>
+              {mediaData?.name}
+            </h1>
 
-          <span className='text-2xl text-gray-500'>
-            {/* the price returned is in cents. Format it to dollars */}
-            {/* ${mediaData?.price as number} */}
-            $ {(mediaData?.price as number / 100).toFixed(2)}
-          </span>
+            <span className='text-md text-zinc-500 font-medium pb-1 border-2 border-zinc-400 rounded-md px-2'>
+              {/* $ {(mediaData?.price as number / 100).toFixed(2)} */}
+              {/* format date as Month Day, Year */}
+              {mediaData?.createdAt ? new Date(mediaData?.createdAt).toLocaleDateString() : ''}
+            </span>
+          </div>
+
+          <img
+            className='object-cover w-100 h-100 rounded-md'
+            src={mediaData?.image ? mediaData?.image : 'https://i.ytimg.com/vi/m-IPERwZyCs/maxresdefault.jpg'}
+            alt={mediaData?.name}
+          />
+          <div className='flex flex-row items-center justify-start w-full md:w-1/2 lg:w-1/3 border-t-2 border-slate-700'>
+            <p className='text-sm text-gray-500 text-left pt-1'>
+              {mediaData?.description}
+            </p>
+          </div>
         </div>
-
-        <img
-          className='object-cover w-full h-full rounded-md'
-          src={mediaData?.image ? mediaData?.image : 'https://via.placeholder.com/150'}
-          alt={mediaData?.name}
-        />
-        <p className='text-sm text-gray-500'>
-          {mediaData?.description}
-        </p>
-      </div>
+      )}
     </>
   )
 }
